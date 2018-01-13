1bd175d911318a2e4cff8ded7388a64aee6d9f3a
Philip Huysmans <philip.huysmans@gmail.com>
2018-01-13 21:57:43 +0100

    added documentation

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,14 +5,13 @@ module.exports = (app) => {
     app.get('/api', (req,res) => res.status(200).send({
         message: 'Welcome to the IoT logger API!'
     }));
-
-    app.post('/api/sensors', sensorsController.create);
+    
     app.get('/api/sensors', sensorsController.list);
     app.get('/api/sensors/:sensorId', sensorsController.retrieve);
+    app.post('/api/sensors', sensorsController.create);
     app.delete('/api/sensors/:sensorId', sensorsController.destroy);
 
     app.get('/api/sensors/:sensorId/sensorreadings', sensorReadingsController.retrieve);
-
     app.post('/api/sensors/:sensorId/sensorreadings', sensorReadingsController.create);
     app.delete('/api/sensors/:sensorId/sensorreadings/:sensorReadingId', sensorReadingsController.destroy);
 }
